16faf2115b881e639e10a470b6442fc815f93a7d
jaylco <77386599+jaylco@users.noreply.github.com>
2022-11-11 08:52:21 +0000

    Removed string conversion on OrganisationalUnitOrderingKey

--- src/main/java/uk/gov/cshr/civilservant/controller/models/OrganisationalUnitOrderingKey.java
+++ src/main/java/uk/gov/cshr/civilservant/controller/models/OrganisationalUnitOrderingKey.java
@@ -1,25 +1,6 @@
 package uk.gov.cshr.civilservant.controller.models;
 
-import org.apache.commons.lang3.EnumUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public enum OrganisationalUnitOrderingKey {
-    NAME("name"),
-    FORMATTED_NAME("formattedName");
-
-    private String value;
-
-    private OrganisationalUnitOrderingKey(String value) {
-        this.value = value;
-    }
-
-    public static OrganisationalUnitOrderingKey fromValue(String value) {
-        for (OrganisationalUnitOrderingKey orderingParam : values()) {
-            if (orderingParam.value.equalsIgnoreCase(value)) {
-                return orderingParam;
-            }
-        }
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("orderBy must be one of %s", EnumUtils.getEnumList(OrganisationalUnitOrderingKey.class)));
-    }
+    NAME,
+    FORMATTED_NAME;
 }
