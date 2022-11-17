1d7a2722dd901fa968565191ba6782f07fa9b861
jaylco <77386599+jaylco@users.noreply.github.com>
2022-11-17 14:01:50 +0000

    Fixed unit test for AgencyTokenCapacityUsed

--- src/main/java/uk/gov/cshr/civilservant/service/identity/model/AgencyTokenCapacityUsed.java
+++ src/main/java/uk/gov/cshr/civilservant/service/identity/model/AgencyTokenCapacityUsed.java
@@ -1,5 +1,10 @@
 package uk.gov.cshr.civilservant.service.identity.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AgencyTokenCapacityUsed {
     public int capacityUsed;
 }
