e056313ab37d37e983ae26e6a0e03402cecd84d2
jaylco <77386599+jaylco@users.noreply.github.com>
2023-10-19 12:18:50 +0100

    Customisable domain regex

--- src/main/java/uk/gov/cshr/civilservant/validation/domain/ValidDomainValidator.java
+++ src/main/java/uk/gov/cshr/civilservant/validation/domain/ValidDomainValidator.java
@@ -1,15 +1,21 @@
 package uk.gov.cshr.civilservant.validation.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import uk.gov.cshr.civilservant.exception.organisationalUnit.InvalidDomainException;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
 
+@Component
 public class ValidDomainValidator  implements ConstraintValidator<ValidDomain, String>  {
+
+    @Value("${domains.validation.regex}")
+    protected String regexPattern;
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        final String regexPattern = "^[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
         if (!Pattern.compile(regexPattern)
                 .matcher(value)
                 .matches()) {
