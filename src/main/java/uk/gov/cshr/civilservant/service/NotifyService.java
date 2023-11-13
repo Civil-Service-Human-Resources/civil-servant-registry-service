47a6f25efeb4061f8dc19d747c4feb5dcf78f9eb
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2023-11-13 10:57:00 +0000

    Remove unused notify() method

--- src/main/java/uk/gov/cshr/civilservant/service/NotifyService.java
+++ src/main/java/uk/gov/cshr/civilservant/service/NotifyService.java
@@ -14,30 +14,12 @@ import uk.gov.service.notify.SendEmailResponse;
 public class NotifyService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(NotifyService.class);
-  private static final String NAME_PERSONALISATION = "name";
-  private static final String LEARNER_PERSONALISATION = "learner";
 
   @Value("${govNotify.key}")
   private String govNotifyKey;
 
   @Value("${govNotify.enabled}")
   private Boolean enabled;
-
-  public void notify(String email, String templateId, String name, String learner)
-      throws NotificationClientException {
-    if (enabled) {
-      HashMap<String, String> personalisation = new HashMap<>();
-      personalisation.put(NAME_PERSONALISATION, name);
-      personalisation.put(LEARNER_PERSONALISATION, learner);
-
-      NotificationClient client = new NotificationClient(govNotifyKey);
-      SendEmailResponse response = client.sendEmail(templateId, email, personalisation, "");
-
-      LOGGER.debug("Line manager notification email: {}", response.getBody());
-    } else {
-      LOGGER.info("Gov Notify disabled - email {}, name {}, learner {}", email, name, learner);
-    }
-  }
 
   public void notify(String email, String templateId, HashMap<String, String> personalisation) throws NotificationClientException {
     if(enabled){
