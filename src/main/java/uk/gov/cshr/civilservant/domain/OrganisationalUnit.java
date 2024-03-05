c2995d02dee8af9cfd412d5604c44ba676aafd00
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-03-05 13:26:05 +0000

    Remove redundant annotation

--- src/main/java/uk/gov/cshr/civilservant/domain/OrganisationalUnit.java
+++ src/main/java/uk/gov/cshr/civilservant/domain/OrganisationalUnit.java
@@ -1,14 +1,12 @@
 package uk.gov.cshr.civilservant.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity
-@Slf4j
 public class OrganisationalUnit extends SelfReferencingEntity<OrganisationalUnit> {
 
     private static final long serialVersionUID = 1L;
