37135075d65829ce44de525c3ea23dc10a2b9d4b
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-04-09 11:14:26 +0100

    New generic method for correctly getting the tree for SelfReferencingEntity objects. Integration test to ensure no bugs

--- src/test/resources/db/migration/h2/V1.999__civil_servant_data.sql
+++ src/test/resources/db/migration/h2/V1.999__civil_servant_data.sql
@@ -1,2 +1,5 @@
 INSERT INTO `identity` (id, uid) VALUES(999, 'learner');
 INSERT INTO civil_servant (identity_id, organisational_unit_id, grade_id, profession_id, full_name) VALUES(999, 2, 1, 1, 'Learner');
+INSERT INTO profession (id, parent_id, name) VALUES(100, 1, 'Analysis-child');
+INSERT INTO profession (id, parent_id, name) VALUES(101, 2, 'Commercial-child');
+INSERT INTO profession (parent_id, name) VALUES(100, 'Analysis-grandchild');
