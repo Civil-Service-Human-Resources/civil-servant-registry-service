5c3c22bcf8b7564e93b4ffc2eeac2debf4917d9e
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-06-05 15:07:07 +0100

    Split SQL statements

--- src/main/java/uk/gov/cshr/civilservant/repository/CivilServantRepository.java
+++ src/main/java/uk/gov/cshr/civilservant/repository/CivilServantRepository.java
@@ -84,9 +84,28 @@ public interface CivilServantRepository extends JpaRepository<CivilServant, Long
                   + "left join Identity i on i.id = c.identity.id "
                   + "left join Grade g on g.id = c.grade.id "
                   + "left join c.otherAreasOfWork oaw "
-                  + "where i.uid in ?1 and (?2 is null or ou.id in ?2) "
+                  + "where i.uid in ?1 and ou.id in ?2 "
                   + "group by c.id ")
-  List<CivilServantReportDto> findAllByUidsNormalised(List<String> uids, List<Long> organisationalUnits);
+  List<CivilServantReportDto> findAllByUidsNormalisedWithOrgs(List<String> uids, List<Long> organisationalUnits);
+
+  @Query(
+          "select new uk.gov.cshr.civilservant.dto.CivilServantReportDto(c.id, c.fullName, ou.code, ou.id, ou.name, p.name, i.uid, g.name, group_concat(oaw.name)) "
+                  + "from CivilServant c "
+                  + "left join OrganisationalUnit ou on ou.id = c.organisationalUnit.id "
+                  + "left join Profession p on p.id = c.profession.id "
+                  + "left join Identity i on i.id = c.identity.id "
+                  + "left join Grade g on g.id = c.grade.id "
+                  + "left join c.otherAreasOfWork oaw "
+                  + "where i.uid in ?1 "
+                  + "group by c.id ")
+  List<CivilServantReportDto> findAllByUidsNormalised(List<String> uids);
+
+  default List<CivilServantReportDto> findAllByUidsNormalised(List<String> uids, List<Long> organisationalUnits) {
+    if (organisationalUnits != null) {
+      return this.findAllByUidsNormalisedWithOrgs(uids, organisationalUnits);
+    }
+    return this.findAllByUidsNormalised(uids);
+  }
 
   @Query(
       "select new uk.gov.cshr.civilservant.dto.CivilServantReportDto(c.id, c.fullName, ou.name, p.name, i.uid, g.name, group_concat(oaw.name)) "
